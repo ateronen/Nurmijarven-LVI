6aba6389086dae8de30f16bd1fa3722c959caf48
Arttu Teronen <arttuteronen@gmail.com>
2023-07-31 12:30:04 +0300

    Updated dev file replacements. Updated environment path in service. Updated socket.io-parser.

--- src/app/services/get-social-data.service.ts
+++ src/app/services/get-social-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 import { SocialPost } from '../components/social-media/social';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
